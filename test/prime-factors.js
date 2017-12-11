79a1b9a5c836dfcf162c5e34fc0cfc19b4bc1832
Troy Rice <troy.rice@samaritanministries.org>
2017-12-11 10:23:23 -0600

    First Commit

---
+++ test/prime-factors.js
@@ -0,0 +1,9 @@
+var expect = require("chai").expect;
+var converter = require("../app/prime-factors");
+
+describe("Prime Pactors", function() {
+  it("Passes", function() {
+    expect(1+1).to.equal(2);
+  });
+
+});
